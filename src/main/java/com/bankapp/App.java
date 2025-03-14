c47709d8141dc6cae8565dd165fcb91132e6d8e4
Panther <desirevai06@gmail.com>
2025-03-14 15:29:57 +0000

    correction de la methode loadUserData dans la classe Connection

--- src/main/java/com/bankapp/App.java
+++ src/main/java/com/bankapp/App.java
@@ -19,7 +19,7 @@ public class App {
     public static void main(String[] args) throws FileNotFoundException {
         App app = new App();
         Connection connect = new Connection();
-        if (connect.login("panther", "password")) {
+        if (connect.login("emily", "password654")) {
             app.session.setCurrentUser(connect.getCurrentUser());
             System.out.println("Bienvenue : " + app.session.getCurrentUser().getLogin());
 
--- src/main/java/com/bankapp/services/Connection.java
+++ src/main/java/com/bankapp/services/Connection.java
@@ -13,18 +13,29 @@ public class Connection {
     private HashMap<String, User> usersDatabase;
     private User currentUser;
 
-    public Connection() throws FileNotFoundException{
+    public Connection() throws FileNotFoundException {
         this.usersDatabase = loadUsersData("src/main/data/users.txt");
     }
+
+    /**
+     * Déconnecte l'utilisateur.
+     */
     public void logout() {
         if (this.currentUser != null) {
             System.out.println("Deconnexion de l'utilisateur: " + this.currentUser.getLogin());
-            this.currentUser = null; 
+            this.currentUser = null;
         } else {
             System.out.println("pas d'utilisateur connecte.");
         }
     }
 
+    /**
+     * Authentifie un utilisateur.
+     * 
+     * @param login
+     * @param password
+     * @return
+     */
     public boolean login(String login, String password) {
         User user = this.usersDatabase.get(login);
         if (user != null && user.getPassword().equals(password)) {
@@ -37,28 +48,42 @@ public class Connection {
         }
     }
 
+    /**
+     * Charge les données des utilisateurs depuis un fichier.
+     * 
+     * @param filePath
+     * @return
+     * @throws FileNotFoundException
+     */
     private HashMap<String, User> loadUsersData(String filePath) throws FileNotFoundException {
         HashMap<String, User> usersMap = new HashMap<>();
         try {
             this.userScanner = new Storage().reader(filePath);
             while (userScanner.hasNextLine()) {
                 String[] parts = this.userScanner.nextLine().split(", ");
-                if (parts.length == 3) {
+
+                if (parts.length == 7) {
                     UserClient client = new UserClient();
                     client.setId(Integer.parseInt(parts[0]));
-                    client.setLogin(parts[1]);
-                    client.setPassword(parts[2]);
-                    usersMap.put(parts[1], client);
+                    client.setLogin(parts[4]);
+                    client.setPassword(parts[5]);
+                    usersMap.put(parts[4], client);
                 }
+
             }
         } finally {
             if (this.userScanner != null) {
-                this.userScanner.close(); 
+                this.userScanner.close();
             }
         }
         return usersMap;
     }
 
+    /**
+     * Récupère l'utilisateur connecté.
+     * 
+     * @return
+     */
     public User getCurrentUser() {
         return this.currentUser;
     }
